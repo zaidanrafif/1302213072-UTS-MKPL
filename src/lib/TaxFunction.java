e496b3d8031fd0b0708476233d00866973c61ebc
zaidanrafif <121496244+zaidanrafif@users.noreply.github.com>
2024-04-24 03:36:32 +0700

    Refactoring agar dapat misahkan validasi pajak
    memisahkan tanggung jawab validasi perhitungan pajak

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -3,6 +3,12 @@ package lib;
 public class TaxFunction {
 
 	
+	private static final int TAX_RATE = 0;
+	private static final int MAX_CHILDREN_FOR_TAX_FREE = 0;
+	private static final int BASE_TAX_FREE_INCOME = 0;
+	private static final int ADDITIONAL_TAX_FREE_INCOME_MARRIED = 0;
+	private static final int ADDITIONAL_TAX_FREE_INCOME_PER_CHILD = 0;
+
 	/**
 	 * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
 	 * 
@@ -15,30 +21,39 @@ public class TaxFunction {
 	 */
 	
 	
-	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-		
-		int tax = 0;
-		
-		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
-		}
-		
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
-		
-		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-		}
-		
-		if (tax < 0) {
-			return 0;
-		}else {
-			return tax;
-		}
-			 
+	 public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
+		validateInput(numberOfMonthWorking, numberOfChildren);
+	
+		int taxFreeIncome = calculateTaxFreeIncome(isMarried, numberOfChildren);
+		int taxableIncome = calculateTaxableIncome(monthlySalary, otherMonthlyIncome, numberOfMonthWorking, deductible, taxFreeIncome);
+	
+		int tax = (int) Math.round(TAX_RATE * taxableIncome);
+	
+		return Math.max(0, tax);
 	}
+	
+	private static void validateInput(int numberOfMonthWorking, int numberOfChildren) {
+		if (numberOfMonthWorking > 12) {
+			throw new IllegalArgumentException("More than 12 month working per year");
+		}
+		if (numberOfChildren > MAX_CHILDREN_FOR_TAX_FREE) {
+			throw new IllegalArgumentException("More than maximum allowed children");
+		}
+	}
+	
+	private static int calculateTaxFreeIncome(boolean isMarried, int numberOfChildren) {
+		int taxFreeIncome = BASE_TAX_FREE_INCOME;
+		if (isMarried) {
+			taxFreeIncome += ADDITIONAL_TAX_FREE_INCOME_MARRIED;
+		}
+		taxFreeIncome += numberOfChildren * ADDITIONAL_TAX_FREE_INCOME_PER_CHILD;
+	
+		return taxFreeIncome;
+	}
+	
+	private static int calculateTaxableIncome(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, int taxFreeIncome) {
+		return ((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - taxFreeIncome;
+	}
+	
 	
 }
